72fcfccb983a197071dabfd4db0ffc3f1c03d08d
Igor-Principal <igorprincipal@gmail.com>
2023-10-26 18:01:34 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -28,23 +28,3 @@ function onClick(evt) {
       captionDelay: 250
     });
 }
-// function onClick(evt) {
-//   evt.preventDefault();
-
-//   const lightbox = new SimpleLightbox(".gallery__item a", {
-//     captionsData: "alt",
-//     captionDelay: 250,
-//   });
-// }
-
-// function createMarkup(arr) {
-//   return arr
-//     .map(
-//       ({ preview, original, description }) => `<li class="gallery__item">
-//    <a class="gallery__link" href="${original}">
-//       <img class="gallery__image" src="${preview}" alt="${description}" />
-//    </a>
-// </li>`
-//     )
-//     .join("");
-// }
